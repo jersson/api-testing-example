23dfeca6368e1ef4657b2fe3a49bb386ea0a0235
jersson <jersson@gmail.com>
2020-07-25 00:43:06 -0500

    fix: use semicolons properly

--- test/controller.test.js
+++ test/controller.test.js
@@ -5,8 +5,8 @@ const Controller = require('../src/controller');
 
 const mockResponse = () => {
     const response = {};
-    response.status = jest.fn().mockReturnValue(response),
-    response.json = jest.fn().mockReturnValue(response)
+    response.status = jest.fn().mockReturnValue(response);
+    response.json = jest.fn().mockReturnValue(response);
 
     return response;
 };
